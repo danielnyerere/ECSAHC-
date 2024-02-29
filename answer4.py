5dfc362e1503cd203c53a91747c78a53c7f26e6c
ibraraj <ibraraj@gmail.com>
2024-02-29 10:53:28 +0300

    fourth question

---
+++ answer4.py
@@ -0,0 +1,12 @@
+
+     public Deck()
+		int cardCount
+		public void shuffle()
+
+     public Card()
+	 
+         public void drawCard(Card c)
+
+         public void returdCard(Card c)
+		 
+	public int getCardCount()
