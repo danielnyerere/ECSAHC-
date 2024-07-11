a4ae2b8dc3a6d77f21e06988b1e77ff21b094d34
danielnyerere <nyereredaniel@gmail.com>
2024-07-11 11:05:16 +0300

    Question 6

---
+++ src/main/java/com/mycompany/ecsa_interview/Answer6.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecsa_interview;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author THPS
+ */
+public class Answer6 {
+    public static int[] twoSumList(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSumList(nums, target);
+        System.out.println(Arrays.toString(result));  // Expected output: [0, 1]
+    }
+}
