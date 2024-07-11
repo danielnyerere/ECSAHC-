a4194a7199823d426c69de1ef8cdef1f32f8c318
danielnyerere <nyereredaniel@gmail.com>
2024-07-11 10:21:10 +0300

    Initial Commit

---
+++ src/main/java/com/mycompany/ecsa_interview/ECSA_Interview.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecsa_interview;
+
+/**
+ *
+ * @author THPS
+ */
+public class ECSA_Interview {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
